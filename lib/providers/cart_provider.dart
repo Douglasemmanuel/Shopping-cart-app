a8428ad87ea7e7e2423c80ff04446cd4ac31773e
DOUGLAS EMMANUEL <emmanueldouglas2121@gmail.com>
2024-11-13 11:20:10 +0000

    made some changes

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -23,8 +23,20 @@ class CartNotifier extends _$CartNotifier {
     }
   }
 
+}
 
+// Dependent provider
 
-  //methods to update state
+@riverpod
+int cartTotal(ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+
+  int total = 0;
+
+  for (Product product in cartProducts) {
+    total += product.price;
+  }
+
+  return total;
 }
 
--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -16,7 +16,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final cartProducts = ref.watch(cartNotifierProvider);
-    // final total = ref.watch(cartTotalProvider);
+    final total = ref.watch(cartTotalProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -45,7 +45,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
             ),
 
             // output totals here
-            // Text('Total price - £$total'),
+            Text('Total price - £$total'),
           ],
         ),
       ),
